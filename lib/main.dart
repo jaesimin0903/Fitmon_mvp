b645b292abafba180f6ba790b32ea62067a37e5c
jaesimin0903 <78476481+jaesimin0903@users.noreply.github.com>
2023-06-04 04:39:02 +0900

    EDIT : Camera

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_chat/models/avatar_data.dart';
 import 'package:flutter_chat/views/home_page.dart';
+import 'package:camera/camera.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final cameras = await availableCameras();
+  final firstCamera = cameras.first;
   runApp(ChangeNotifierProvider(
     create: (context) => AvatarData(),
-    child: MyApp(),
+    child: MyApp(camera: firstCamera),
   ));
 }
 
 class MyApp extends StatelessWidget {
+  final CameraDescription camera;
+
+  MyApp({required this.camera});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
+      home: MyHomePage(camera: camera),
     );
   }
 }
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -4,9 +4,11 @@ import 'shop_page.dart';
 import 'user_page.dart';
 import 'excercise_page.dart';
 import 'camera_page.dart';
+import 'package:camera/camera.dart';
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key}) : super(key: key);
+  final CameraDescription camera;
+  MyHomePage({Key? key, required this.camera}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -14,16 +16,16 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _currentIndex = 0;
-  final List<Widget> _children = [
-    CategoryPage('Category 1'),
-    ShopPage('Category 2'),
-    CameraScreen(),
-    WorkoutPage(),
-    RankingPage(),
-  ];
 
   @override
   Widget build(BuildContext context) {
+    final List<Widget> _children = [
+      CategoryPage('Category 1'),
+      ShopPage('Category 2'),
+      CameraScreen(camera: widget.camera),
+      WorkoutPage(),
+      RankingPage(),
+    ];
     return Scaffold(
       body: _children[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
